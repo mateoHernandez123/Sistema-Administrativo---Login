cc4f08326b81d0e9d4f93e2bc518a9f7e0d59146
MatiasFochi <matiasfochi.dev@gmail.com>
2024-08-30 20:46:10 -0300

    Login conectado al backend

--- login/src/LoginPage.jsx
+++ login/src/LoginPage.jsx
@@ -2,6 +2,10 @@
 import React, { useState } from 'react';
 import './style.css'; // Asegúrate de que el archivo style.css esté en la misma carpeta o ajusta la ruta según corresponda
 import Swal from 'sweetalert2'; //Libreria de alertas y popups personalizadas
+import CryptoJS from'crypto-js';
+
+const ip = 'localhost:5000';
+
 const Toast = Swal.mixin({
   toast: true,
   position: "top-end",
@@ -14,26 +18,71 @@ const Toast = Swal.mixin({
   }
 }); //seteo de la alerta que aparece en el login
 
+
+function encriptar(password) {
+  const hash = CryptoJS.SHA256(password);
+  const textoCifrado = hash.toString(CryptoJS.enc.Hex);
+  return textoCifrado;
+};
+
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
 
+  async function login(mail, clave) {
+    const hashClave = encriptar(clave);
+    let fetchLogin = await fetch(`http://${ip}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        Usuario: mail,
+        Passw: hashClave
+      })
+    });
+
+    fetchLogin = await fetchLogin.json();
+
+    return fetchLogin;
+  }
+
+
   const handleSubmit = (e) => {
     e.preventDefault(); // Evita el envío del formulario
 
-    // Ejemplo de validación de contraseña (puedes ajustar la lógica según tus necesidades)
-    if (password.length < 6) {
-      setError(true); // Muestra el mensaje de error
-    } else {
-      setError(false); // Oculta el mensaje de error
+    // Aquí podrías manejar el envío del formulario, como hacer una petición a un servidor
+    login(email, password)
+    .then((resultadoLogin)=>{
+      if(resultadoLogin.ERROR){
+        setError(false);
+        console.log(resultadoLogin.MENSAJE)
+      }else{
+        if(resultadoLogin.ESTADO){
+          setError(false);
+          Toast.fire({
+            icon:'success',
+            title:'Sesión iniciada',
+            text: resultadoLogin.MENSAJE
+          }) //Disparo de la alerta login exitoso
+        } else{
+          setError(true);
+          Toast.fire({
+            icon:'error',
+            title:'Error de credenciales',
+            text: resultadoLogin.MENSAJE
+          }) //Disparo de la alerta login exitoso
+        }
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
       Toast.fire({
-        icon:'success',
-        title:'Sesión iniciada',
-        text: email
-      }) //Disparo de la alerta
-      // Aquí podrías manejar el envío del formulario, como hacer una petición a un servidor
-    }
+        icon:'error',
+        title:'Error inseperado.'
+      })
+    })
   };
 
   return (
