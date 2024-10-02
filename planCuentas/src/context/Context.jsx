06866f823a1289c46a538a69ed25eaeb00d81af1
Mateo Hernandez <mateo.e.2003hernandez@gmail.com>
2024-10-02 19:41:34 -0300

    Usando Context

---
+++ planCuentas/src/context/Context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+
+export const Context = createContext();
+
+const estadoUsuario =
+  JSON.parse(localStorage.getItem("UsuarioAutenticado")) || false;
+
+export const ContextProvider = ({ children }) => {
+  
+  const [usuarioAutenticado, setUsuarioAutenticado] = useState(estadoUsuario);
+
+  const logear = (user, token) => {
+    localStorage.setItem("accessToken", JSON.stringify(token));
+    localStorage.setItem("Usuario", JSON.stringify(user));
+    setUsuarioAutenticado(true);
+  };
+
+  const deslogear = () => {
+    localStorage.clear();
+    setUsuarioAutenticado(false);
+  };
+
+  const tokenError = (mensaje) => {
+    /*Alerta.fire({
+      icon:'warning',
+      title: 'Atención!',
+      text: mensaje+'. Debe volver a registrarse.'
+    })*/ //Aca hay que definirle algun popup para mostrar que hubo un error
+    deslogear();
+  };
+
+  useEffect(() => {
+    localStorage.setItem(
+      "UsuarioAutenticado",
+      JSON.stringify(usuarioAutenticado)
+    );
+  }, [usuarioAutenticado]);
+  return (
+    <Context.Provider
+      value={{ usuarioAutenticado, logear, deslogear, tokenError }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
